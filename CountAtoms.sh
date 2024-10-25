be250805931b1447fbd905e841058036b55b5b6c
root <root@DESKTOP-JDPLISO.>
2024-10-25 19:44:40 +0200

    Count atom C and H

--- CountAtoms.sh
+++ CountAtoms.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-filename="molecules/cubane.pdb"
-
-# Create a txt to store names
-echo C > names.txt
+filename=("molecules/*")
 
 # In sed, eliminate the first 2 lines and the last line (Just for practice)
 function renames(){
-	atomnames=$(sed -e '1,2d' -e '$d' $1 |
-		awk '/ATOM/{print $3}' |
-		awk '/C/{print $1}' |
-		wc -l)
-	echo $atomnames >> names.txt
+    atomnames=$(sed -e '1,2d' -e '$d' "$1" |
+                awk -v atom="$2" '/ATOM/ && $3 == atom {print $3}' |
+                wc -l)
+    echo "$atomnames" >> names.txt
 }
 
-renames "$filename"
+
+for file in molecules/*; do
+	echo C > names.txt
+	renames "$file" "C"
+	echo H >> names.txt
+	renames "$file" "H"
+# If the molecule contains atom O
+#	echo O >> names.txt
+#	renames "$file" "O"
+
+	newname=$(awk '{printf $0}' names.txt)
 
 
-#cat $filename
+	cp "$file" "$newname" 
+	done
+rm names.txt
+
